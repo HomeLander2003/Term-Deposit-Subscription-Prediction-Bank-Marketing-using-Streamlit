7409cdfabe4874c3247d7f3206448fa8b3869b1f
Bilal Ahmed <bilallahmedd685@gmail.com>
2025-08-09 19:32:30 +0500

    Create stream.py

---
+++ script/stream.py
@@ -0,0 +1,28 @@
+
+class stream(deployment):
+    
+    def run_eda(self):
+        self.clean()
+        self.save()
+        self.analysis()
+        self.visualize()
+        
+    def run_fe_ml(self):
+        self.clean()
+        self.FE()
+        self.deploy()
+
+
+        
+    def app(self):
+
+        st.sidebar.title("Model Options")
+
+        options = {
+            "EDA ":self.run_eda,
+            "FE + ML": self.run_fe_ml
+        }
+
+        opt_name = st.sidebar.selectbox("Choose Options", list(options.keys()))
+        selected_opt = options[opt_name]
+        selected_opt()  
